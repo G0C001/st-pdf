7ed6a374455ebed64eda55ce83ce48b8d66a4039
G0C001 <gokulvbe1303@gmail.com>
2024-11-05 17:00:40 +0530

    h

--- install_streamlit_weasyprint.sh
+++
@@ -1,9 +0,0 @@
-sudo apt update
-sudo apt install -y libglib2.0-0 libpango-1.0-0 libfontconfig1 libpangoft2-1.0-0
-
-python3 -m venv myenv
-source myenv/bin/activate
-
-pip install streamlit WeasyPrint
-
-echo "Installation completed. You can activate your environment using 'source myenv/bin/activate'."
