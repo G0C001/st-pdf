238f2c014f8905eb91cf31766873ba016f5e8075
G0C001 <gokulvbe1303@gmail.com>
2024-11-05 18:04:16 +0530

    h

--- body/personal.py
+++ body/personal.py
@@ -8,8 +8,8 @@ def personal(role):
                 <div class="contact-info">
                     <p>Phone: 7339557072</p>
                     <p>Email: <a href="mailto:gokulvbe1303@gmail.com">gokulvbe1303@gmail.com</a></p>
-                    <p>LinkedIn: <a href="https://www.linkedin.com/in/gokul-vasanth-d-18a228222/" target="_blank">https://www.linkedin.com/in/gokul-vasanth-d-18a228222/</a></p>
-                    <p>Portfolio: <a href="https://g0c001.github.io/Portfolio/" target="_blank">https://g0c001.github.io/Portfolio/</a></p>
+                    <p>LinkedIn: <a href="https://www.linkedin.com/in/gokul-vasanth-d-18a228222/" target="_blank">www.linkedin.com/in/gokul/</a></p>
+                    <p>Portfolio: <a href="https://g0c001.github.io/Portfolio/" target="_blank">www.Portfolio.com/in/gokul/</a></p>
                 </div>
                 </section>"""
     return personal
